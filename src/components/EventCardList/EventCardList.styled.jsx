9e4a264e8be5497b81896efc3949a3f0bb815ee1
MathiasHobler <44622193+MathiasHobler@users.noreply.github.com>
2022-06-23 10:50:19 +0200

    fix code quality

--- src/components/EventCardList/EventCardList.styled.jsx
+++ src/components/EventCardList/EventCardList.styled.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const EventListContainer = styled.ul`
-	width: min(100% - 2rem);
 	display: flex;
-	margin: auto;
 	flex-direction: column;
+	width: min(100% - 2rem);
+	margin: auto;
 	row-gap: 1.5em;
 `;
 
