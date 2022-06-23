598d32e028a70e36d0321964b6f98b769758eaef
Falco Marecek <103875201+Falconiac@users.noreply.github.com>
2022-06-23 12:13:19 +0200

    Import Error
    Imports changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import {Helmet} from 'react-helmet';
 
-import Form from '../components/Form/Form';
 import EventCard from '../components/EventCard/EventCard';
+import Form from '../components/Form/Form';
 import Layout from '../components/Layout';
 import dummyEvent from '../utils/data';
 
