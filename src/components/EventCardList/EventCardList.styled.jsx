f907fdf022c614c22ddc78330ef4306153366345
MathiasHobler <44622193+MathiasHobler@users.noreply.github.com>
2022-06-23 10:47:09 +0200

    fix stylelint error

--- src/components/EventCardList/EventCardList.styled.jsx
+++ src/components/EventCardList/EventCardList.styled.jsx
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const EventListContainer = styled.ul`
 	width: min(100% - 2rem);
-	margin: auto;
 	display: flex;
+	margin: auto;
 	flex-direction: column;
 	row-gap: 1.5em;
 `;
