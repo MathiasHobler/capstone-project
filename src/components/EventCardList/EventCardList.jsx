aa88032a825abbb8d0964fa93a4fd0e4d333c224
MathiasHobler <44622193+MathiasHobler@users.noreply.github.com>
2022-07-14 14:48:06 +0200

    push fetch into zustand

--- src/components/EventCardList/EventCardList.jsx
+++ src/components/EventCardList/EventCardList.jsx
@@ -8,31 +8,36 @@ import EventListContainer from './EventCardList.styled';
 const EventCardList = ({eventList}) => {
 	const [{data, error}, setData] = useState({data: [], error: null});
 	const setEvents = useEvents(state => state.setEvents);
+	const fetchData = useEvents(state => state.fetchData);
+	const {events} = useEvents(state => state.events);
 
 	const [render, newRender] = useState('');
 
 	useEffect(() => {
-		fetch('/api/events')
-			.then(response => {
-				if (!response.ok) {
-					throw Error(response.statusText);
-				} else {
-					return response.json();
-				}
-			})
-			.then(data => {
-				setData({
-					data: data.data,
-					error: null,
-				});
-				setEvents(data.data);
-			})
-			.catch(error => {
-				setData({
-					data: [],
-					error: error.message,
-				});
-			});
+		fetchData();
+		console.log(events);
+		// fetch('/api/events')
+		// 	.then(response => {
+		// 		if (!response.ok) {
+		// 			throw Error(response.statusText);
+		// 		} else {
+		// 			return response.json();
+		// 		}
+		// 	})
+		// 	.then(data => {
+		// 		console.log(data);
+		// 		setData({
+		// 			data: data.data,
+		// 			error: null,
+		// 		});
+		// 		setEvents(data.data);
+		// 	})
+		// 	.catch(error => {
+		// 		setData({
+		// 			data: [],
+		// 			error: error.message,
+		// 		});
+		// 	});
 	}, [render]);
 
 	function deleteEvent(id) {
@@ -86,7 +91,8 @@ const EventCardList = ({eventList}) => {
 	return (
 		<EventListContainer data-testid="list">
 			{error && <div>An error occured: {error}</div>}
-			{data.map(singleEvent => {
+			{console.log(events)}
+			{events.map(singleEvent => {
 				return (
 					<EventCard
 						data-testid="listItem"
@@ -97,6 +103,17 @@ const EventCardList = ({eventList}) => {
 					></EventCard>
 				);
 			})}
+			{/* {data.map(singleEvent => {
+				return (
+					<EventCard
+						data-testid="listItem"
+						key={singleEvent._id}
+						event={singleEvent}
+						deleteEvent={deleteEvent}
+						bookmark={bookmarkEvent}
+					></EventCard>
+				);
+			})} */}
 		</EventListContainer>
 	);
 };
--- src/hooks/useEvents.js
+++ src/hooks/useEvents.js
@@ -4,7 +4,46 @@ import {persist} from 'zustand/middleware';
 export const useEvents = create(
 	persist(
 		set => ({
-			events: [],
+			events: {
+				events: [],
+				loading: null,
+				error: null,
+			},
+			fetchData() {
+				set(state => ({
+					events: {
+						events: state.events.events,
+						loading: true,
+						error: null,
+					},
+				}));
+				fetch('/api/events')
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						} else {
+							return response.json();
+						}
+					})
+					.then(data => {
+						set(() => ({
+							events: {
+								events: data.data,
+								loading: false,
+								error: null,
+							},
+						}));
+					})
+					.catch(error => {
+						set(state => ({
+							events: {
+								events: state.events.events,
+								loading: false,
+								error: error.message,
+							},
+						}));
+					});
+			},
 			setEvents: input => set(() => ({events: input})),
 		}),
 		{
