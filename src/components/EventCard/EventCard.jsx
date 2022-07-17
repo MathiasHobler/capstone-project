45d80306043eabf0a4683ac4eb0271c1625f9be0
MathiasHobler <44622193+MathiasHobler@users.noreply.github.com>
2022-07-17 20:40:15 +0200

    show same date style on eventcard

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -6,6 +6,7 @@ import EventCardContainer from './EventCard.styled';
 
 const EventCard = ({event, deleteEvent, bookmark}) => {
 	const [details, setDetails] = useState(false);
+	const date = new Date(event.date);
 
 	const toggleDetail = () => {
 		setDetails(!details);
@@ -23,7 +24,7 @@ const EventCard = ({event, deleteEvent, bookmark}) => {
 				<article>
 					<h2 aria-level="2">{event.title}</h2>
 					<p className="description">{event.description}</p>
-					<p className="date">{event.date}</p>
+					<p className="date">{date.toDateString()}</p>
 				</article>
 			</EventCardContainer>
 
