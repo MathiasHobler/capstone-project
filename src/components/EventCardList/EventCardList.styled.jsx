dfed88546bf691edc2545cfd18eaa11f00e7e9d7
MathiasHobler <44622193+MathiasHobler@users.noreply.github.com>
2022-07-17 19:56:42 +0200

    fix last li behind navbar

--- src/components/EventCardList/EventCardList.styled.jsx
+++ src/components/EventCardList/EventCardList.styled.jsx
@@ -4,10 +4,9 @@ const EventListContainer = styled.ul`
 	display: flex;
 	flex-direction: column;
 	width: min(100% - 2rem);
-	height: 91vh;
 	row-gap: 1.5em;
 	margin: 4.5em auto 10em auto;
-	padding: 0;
+	padding-bottom: 5em;
 `;
 
 export default EventListContainer;
